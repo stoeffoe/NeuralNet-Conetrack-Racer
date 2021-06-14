1a2c94304c7d90391de695f8c7b0007af94c0d52
Ruben Hiemstra <rchiemstra.rh@gmail.com>
2021-06-14 16:55:49 +0200

    refactored/documented controls

---
+++ src/main/java/AutoCoureur/App.java
@@ -0,0 +1,75 @@
+package AutoCoureur;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+import CarSimulator.Car;
+import CarSimulator.Properties;
+import Machine_Learning.*;
+/**
+ * Requirements:
+ * 
+ * 
+ * Design:
+ * 
+ * 
+ * Testing:
+ * 
+ * 
+ */
+public class App {
+
+    public static void main(String[] args) {
+        // Dataset d = new Dataset();
+
+        // int[] layers = { 8, 6, 4, 1 };
+        // NeuralNet nn = new NeuralNet(layers);
+
+        // nn.fit(d.getDataSet(), 1, 10000);
+        createData();
+
+    }
+
+    
+
+    public static void createData(){
+        Car car = new Car();
+        UserInputControls kc = UserInputControls.getInstance();
+        while (true) {
+            Properties x = car.recvProperties();
+
+            double a = kc.getSteeringAngle();
+            double v = kc.getTargetVelocity();
+
+            car.sendControls(a, v);
+
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            String text = "dataSet.add(new Data(new double[]" +  Arrays.toString(x.getLidarDistances()).replace("[", "{").replace("]", "}") + ", {" + a + "}" + "));"; // dataSet.add(new Data(c0, cross));//0;
+            try {
+                whenAppendToFileUsingFileWriter_thenCorrect("drive.txt", text);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void whenAppendToFileUsingFileWriter_thenCorrect(String fileName, String input) throws IOException {
+
+        FileWriter fw = new FileWriter(fileName, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(input);
+        bw.newLine();
+        bw.close();
+
+    }
+
+}
--- src/main/java/AutoCoureur/UserInputControls.java
+++ src/main/java/AutoCoureur/UserInputControls.java
@@ -10,6 +10,7 @@ import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseMotionListener;
 
+
 public final class UserInputControls implements NativeKeyListener, NativeMouseMotionListener {
 
     private static UserInputControls singletonInstance;
@@ -22,8 +23,10 @@ public final class UserInputControls implements NativeKeyListener, NativeMouseMo
 
     private static double speedIncrement = 0.45;
     
+
+
     /**
-     * Private constructor, 
+     * private constructor, 
      * initializes the system wide controls to control the car
      */
     private UserInputControls() {
@@ -39,8 +42,9 @@ public final class UserInputControls implements NativeKeyListener, NativeMouseMo
         }
     }
 
+
     /**
-     * @return Singleton instance
+     * @return singleton instance
      */
     public static UserInputControls getInstance() {
         if (singletonInstance == null) {
@@ -49,6 +53,8 @@ public final class UserInputControls implements NativeKeyListener, NativeMouseMo
 
         return singletonInstance;
     }
+
+
 
     /**
      * @return Desired steering angle from user input
@@ -64,6 +70,7 @@ public final class UserInputControls implements NativeKeyListener, NativeMouseMo
         return targetVelocity;
     }
 
+
     @Override
     public void nativeKeyPressed(NativeKeyEvent ev) {
         switch(ev.getKeyCode()){
@@ -77,19 +84,20 @@ public final class UserInputControls implements NativeKeyListener, NativeMouseMo
         }
     }
 
+    
     @Override
-    public void nativeMouseMoved(NativeMouseEvent mouse) {
-	this.steeringAngle = (mouse.getX() - (screenWidth/2)) * steeringFactor;
-    }
+	public void nativeMouseMoved(NativeMouseEvent mouse) {
+		this.steeringAngle = (mouse.getX() - (screenWidth/2)) * steeringFactor;
+	}
 
-    // Unused mandatory methods
+
     @Override
     public void nativeKeyReleased(NativeKeyEvent e) {}
 
     @Override
     public void nativeKeyTyped(NativeKeyEvent e) {}
 
-    @Override
-    public void nativeMouseDragged(NativeMouseEvent e) {}
+	@Override
+	public void nativeMouseDragged(NativeMouseEvent e) {}
 }
 
