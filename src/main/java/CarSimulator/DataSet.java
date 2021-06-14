971f4c6e97cfee10512b0dd57f6e591e91dc24d9
Jordy Weijgertse <0974347@hr.nl>
2021-06-14 15:45:18 +0200

    added javadoc

--- src/main/java/CarSimulator/DataSet.java
+++ src/main/java/CarSimulator/DataSet.java
@@ -7,6 +7,7 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
@@ -16,56 +17,91 @@ public class DataSet{
     private LinkedList<Properties> propertiesList;
     private LinkedList<Controls> controlsList;
 
+    /**
+     * Initialize a dataset with an empty list of properties and an empty list of controls
+     */
     public DataSet(){
         gson = new Gson();
         propertiesList = new LinkedList<Properties>();
         controlsList = new LinkedList<Controls>();
     }
 
+    /**
+     * Add a properties object to the end of the list
+     * @param properties
+     */
     public void addProperties(Properties properties){
         propertiesList.add(properties);
     }
 
-    public LinkedList<Properties> getPropertiesList() {
+    /**
+     * 
+     * @return The list of properties objects
+     */
+    public LinkedList<Properties> getPropertiesList(){
         return propertiesList;
     }
     
-    public Properties getFirstProperties(){
+    /**
+     * 
+     * @return Pop the first properties object from the list
+     * @throws NoSuchElementException When the list is empty
+     */
+    public Properties getFirstProperties() throws NoSuchElementException{
         return propertiesList.removeFirst();
     }
 
+    /**
+     * Add a controls object to the end of the list
+     * @param controls
+     */
     public void addControls(Controls controls){
         controlsList.add(controls);
     }
 
-    public LinkedList<Controls> getControlsList() {
+    /**
+     * 
+     * @return The list of controls objects
+     */
+    public LinkedList<Controls> getControlsList(){
         return controlsList;
     }
 
-    public Controls getFirstControls(){
+    /**
+     * 
+     * @return Pop the first controls object from the list
+     * @throws NoSuchElementException When the list is empty
+     */
+    public Controls getFirstControls() throws NoSuchElementException{
         return controlsList.removeFirst();
     }
 
+    /**
+     * Save the lists within this object to a json file
+     * @param fileName The location and name of the json file where the data needs to be saved to
+     */
     public void saveToJsonFile(String fileName){
         try{
             Writer writer = new FileWriter(fileName);
             gson.toJson(this, writer);
             writer.close();
-        } catch (JsonIOException e){
-            // TODO Auto-generated catch block
+        } catch(JsonIOException e){
             e.printStackTrace();
-        } catch (IOException e){
-            // TODO Auto-generated catch block
+        } catch(IOException e){
             e.printStackTrace();
         }
     }
 
+    /**
+     * Load a dataset object from a json file
+     * @param fileName The location and name of the file where the json info needs to be loaded from
+     * @return A dataset object with the lists in it
+     */
     public DataSet loadFromJsonFile(String fileName){
         try {
             Reader reader = Files.newBufferedReader(Paths.get(fileName));
             return gson.fromJson(reader, DataSet.class);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch(IOException e){
             e.printStackTrace();
         }
         return null;
--- src/test/java/CarSimulator/DataSetTest.java
+++ src/test/java/CarSimulator/DataSetTest.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
