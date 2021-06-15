c3f15ae76f2939ab59d1e20ad6ce23145b155de9
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-15 12:29:46 +0200

    reordered methods

--- src/main/java/NeuralNet/MatMath.java
+++ src/main/java/NeuralNet/MatMath.java
@@ -16,15 +16,24 @@ public final class MatMath {
     private MatMath(){} // private constructor; no initialization, only static methods
 
 
-    public static double[][] sub(double[][] mA, double[][] mB) {
+    public static double[][] multiplyAndActivate(double[][] mA, double[][] mB, ActivationFunction af) throws IllegalArgumentException {
+        
+        if (!areMultiplyable(mA, mB)) throw new IllegalArgumentException("Matrix sizes are not compatible");
+
         int rows = mA.length;
-        int cols = mA[0].length;
+        int cols = mB[0].length;
+        int numOfCalcs = mA[0].length;
 
-        double[][] resultMatrix = new double[cols][rows];
+        double[][] resultMatrix = new double[rows][cols];
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                resultMatrix[c][r] = mA[r][c] - mB[r][c];
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+
+                double sum = 0;
+                for (int i=0; i<numOfCalcs; i++){
+                    sum += mA[r][i] * mB[i][c];
+                }
+                resultMatrix[r][c] = af.calculateActivation(sum);
             }
         }
 
@@ -54,6 +63,22 @@ public final class MatMath {
 
         return sse;
     }
+
+
+    public static double[][] sub(double[][] mA, double[][] mB) {
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double[][] resultMatrix = new double[cols][rows];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                resultMatrix[c][r] = mA[r][c] - mB[r][c];
+            }
+        }
+
+        return resultMatrix;
+    }
     
 
     public static double[][] fromList(double[] list){
@@ -67,31 +92,6 @@ public final class MatMath {
 
     public static void print(double[][] mA) {
         System.out.println(Arrays.deepToString(mA));
-    }
-    
-    
-    public static double[][] multiplyAndActivate(double[][] mA, double[][] mB, ActivationFunction af) throws IllegalArgumentException {
-        
-        if (!areMultiplyable(mA, mB)) throw new IllegalArgumentException("Matrix sizes are not compatible");
-
-        int rows = mA.length;
-        int cols = mB[0].length;
-        int numOfCalcs = mA[0].length;
-
-        double[][] resultMatrix = new double[rows][cols];
-
-        for (int r=0; r<rows; r++){
-            for (int c=0; c<cols; c++){
-
-                double sum = 0;
-                for (int i=0; i<numOfCalcs; i++){
-                    sum += mA[r][i] * mB[i][c];
-                }
-                resultMatrix[r][c] = af.calculateActivation(sum);
-            }
-        }
-
-        return resultMatrix;
     }
 
 
