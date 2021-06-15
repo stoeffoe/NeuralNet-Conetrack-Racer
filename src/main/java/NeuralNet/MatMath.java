9ce55c2302f80d67b1caf38b257c47e814c79132
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-15 12:29:46 +0200

    reordered methods

--- src/main/java/NeuralNet/MatMath.java
+++ src/main/java/NeuralNet/MatMath.java
@@ -16,53 +16,6 @@ public final class MatMath {
     private MatMath(){} // private constructor; no initialization, only static methods
 
 
-    public static double[][] sub(double[][] mA, double[][] mB) {
-        int rows = mA.length;
-        int cols = mA[0].length;
-
-        double[][] resultMatrix = new double[cols][rows];
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                resultMatrix[c][r] = mA[r][c] - mB[r][c];
-            }
-        }
-
-        return resultMatrix;
-    }
-
-
-    public static double sumSquaredErrors(double[][] mA, double[][] mB) {
-        int rows = mA.length;
-        int cols = mA[0].length;
-
-        double sse = 0;
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                double error = mA[r][c] - mB[r][c];
-                sse += error*error;
-            }
-        }
-
-        return sse;
-    }
-    
-
-    public static double[][] fromList(double[] list){
-        double[][] newVector = new double[list.length][1];
-        for (int i=0; i<list.length; i++) {
-            newVector[i][0] = list[i];
-        }
-        return newVector;
-    }
-
-
-    public static void print(double[][] mA) {
-        System.out.println(Arrays.deepToString(mA));
-    }
-    
-    
     public static double[][] multiplyAndActivate(double[][] mA, double[][] mB, ActivationFunction af) throws IllegalArgumentException {
         
         if (!areMultiplyable(mA, mB)) throw new IllegalArgumentException("Matrix sizes are not compatible");
@@ -85,6 +38,53 @@ public final class MatMath {
         }
 
         return resultMatrix;
+    }
+
+
+    public static double sumSquaredErrors(double[][] mA, double[][] mB) {
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double sse = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                double error = mA[r][c] - mB[r][c];
+                sse += error*error;
+            }
+        }
+
+        return sse;
+    }
+
+
+    public static double[][] sub(double[][] mA, double[][] mB) {
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double[][] resultMatrix = new double[cols][rows];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                resultMatrix[c][r] = mA[r][c] - mB[r][c];
+            }
+        }
+
+        return resultMatrix;
+    }
+    
+
+    public static double[][] fromList(double[] list){
+        double[][] newVector = new double[list.length][1];
+        for (int i=0; i<list.length; i++) {
+            newVector[i][0] = list[i];
+        }
+        return newVector;
+    }
+
+
+    public static void print(double[][] mA) {
+        System.out.println(Arrays.deepToString(mA));
     }
 
 
