e237f339c54f12a1ae30f07e47e6490e652e4a6e
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-15 11:47:31 +0200

    optimized sum

--- src/main/java/NeuralNet/MatMath.java
+++ src/main/java/NeuralNet/MatMath.java
@@ -35,7 +35,19 @@ public final class MatMath {
             }
         }
 
-        return resultMatrix;
+    public static double sum(double[][] mA){
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double sum = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                sum += mA[r][c];
+            }
+        }
+
+        return sum;
     }
 
 
