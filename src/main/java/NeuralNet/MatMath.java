7b6577b8a401200d783a2d915bd8e3b4a885f2f4
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-15 11:51:22 +0200

    formatting

--- src/main/java/NeuralNet/MatMath.java
+++ src/main/java/NeuralNet/MatMath.java
@@ -14,19 +14,9 @@ public final class MatMath {
     private MatMath(){} // private constructor; no initialization, only static methods
 
 
-<<<<<<< HEAD
-    public static double[][] multiplyAndActivate(double[][] mA, double[][] mB, ActivationFunction af) throws IllegalArgumentException {
-        
-        if (!areMultiplyable(mA, mB)) throw new IllegalArgumentException("Matrix sizes are not compatible");
-
-        int rows = mA.length;
-        int cols = mB[0].length;
-        int numOfCalcs = mA[0].length;
-=======
     public static double[][] pow(double[][] mA, double n) {
         return map(mA, a -> Math.pow(a,n));
     }
->>>>>>> db57197 (removed norm and magnitude (not used))
 
         double[][] resultMatrix = new double[rows][cols];
 
@@ -53,7 +43,7 @@ public final class MatMath {
             }
         }
 
-    public static double sum(double[][] mA){
+    public static double sum(double[][] mA) {
         int rows = mA.length;
         int cols = mA[0].length;
 
@@ -133,8 +123,77 @@ public final class MatMath {
     }
 
 
+<<<<<<< HEAD
     public static void print(double[][] mA) {
         System.out.println(Arrays.deepToString(mA));
+=======
+    public static double[][] map(double[][] mA, double[][] mB, BiFunction<Double, Double, Double> fn) throws IllegalArgumentException { 
+        
+        if (!matricesEqualSize(mA, mB)) throw new IllegalArgumentException("Matrices must be equal in size");
+
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double[][] resultMatrix = new double[rows][cols];
+
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+                resultMatrix[r][c] = fn.apply(mA[r][c], mB[r][c]);
+            }
+        }
+
+        return resultMatrix;
+    }
+
+
+    public static double[][] map(double[][] mA, Function<Double, Double> fn) { 
+        int rows = mA.length;
+        int cols = mA[0].length;
+
+        double[][] resultMatrix = new double[rows][cols];
+
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+                resultMatrix[r][c] = fn.apply(mA[r][c]);
+            }
+        }
+
+        return resultMatrix;
+    }
+
+
+    public static String toString(double[][] mat) {
+        return Arrays.deepToString(mat);
+    }
+
+
+    public static void print(double[][] mA) {
+        System.out.println(toString(mA));
+    }
+    
+    
+    public static double[][] multiply(double[][] mA, double[][] mB) throws IllegalArgumentException {
+        
+        if (!areMultiplyable(mA, mB)) throw new IllegalArgumentException("Matrix sizes are not compatible");
+
+        int rows = mA.length;
+        int cols = mB[0].length;
+        int numOfCalcs = mA[0].length;
+
+        double[][] resultMatrix = new double[rows][cols];
+
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+
+                for (int i=0; i<numOfCalcs; i++){
+                    resultMatrix[r][c] += mA[r][i] * mB[i][c];
+                }
+
+            }
+        }
+
+        return resultMatrix;
+>>>>>>> 8f6e221 (formatting)
     }
 
 
