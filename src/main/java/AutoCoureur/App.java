06698cb09d9aa45f1d73b05aa9447fdef258e9b5
Ruben Hiemstra <rchiemstra.rh@gmail.com>
2021-06-17 16:11:59 +0200

    app

--- src/main/java/AutoCoureur/App.java
+++ src/main/java/AutoCoureur/App.java
@@ -24,10 +24,11 @@ import NeuralNet.NeuralNet;
  */
 public class App {
     private static int amountOfRays = 4;
-    private static double minDistance = 0.5;
-    private static double maxDistance = 5;
+    private static double minDistance = .4;
+    private static double maxDistance =4;
     private static double maxSteeringAngle = 45;
-    private static int[] layers = {amountOfRays, 6, 4, 1};
+    private static int[] layers = {amountOfRays,8,3,1 }; 
+    private static double facSteeringAngle = 2.5;
 
 
     public static void main(String[] args){
@@ -91,6 +92,7 @@ public class App {
                 carData.addProperties(properties);
             }
 
+            
             double steeringAngle = uic.getSteeringAngle();
             double targetVelocity = uic.getTargetVelocity();
             Controls controls = car.sendControls(steeringAngle, targetVelocity);
@@ -141,7 +143,7 @@ public class App {
 
         UserInputControls uic = UserInputControls.getInstance();
         System.out.println("NeuralNet.cores "+ NeuralNet.cores);
-        System.out.println("press ESC to save the NN and exit the program");
+        System.out.println("press PAGE_UP to save the NN and exit the program");
         
         int count = 0;
         double weights = 1;
@@ -149,10 +151,12 @@ public class App {
         while(!uic.getQuitingStatus()){             
             double LowestError = nn.fit(dataSet, weights, 100,count);
             double errorDiff = oldLowestError - LowestError;
-            System.out.println("errorDiff "+  errorDiff );
+            System.out.println("LowestError "+ Math.sqrt(LowestError) );
 
             if(errorDiff ==0){
-                weights/=10;
+                weights/=10; 
+                System.out.println("\n weights has been changed to "+ weights);
+                if(weights  == 0.01){break;}
             }
 
             oldLowestError =  LowestError;
@@ -190,7 +194,7 @@ public class App {
             double steeringAngle = MatMath.denormalize(neuralNet.predict(neuralNetInput)[0][0], -maxSteeringAngle, maxSteeringAngle);
             double targetVelocity = 0.9;    // default velocity, to be replaced by the neuralnet
 
-            car.sendControls(steeringAngle, targetVelocity);
+            car.sendControls(steeringAngle*facSteeringAngle, targetVelocity);
         }
     }
 
