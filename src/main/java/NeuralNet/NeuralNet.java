a4403c51973e918932c991a12ac3786a9ea07dbb
Stefan Beenen <Stefan.Beenen@hotmail.com>
2021-06-20 13:37:36 +0200

    removed unused imports

--- src/main/java/NeuralNet/NeuralNet.java
+++ src/main/java/NeuralNet/NeuralNet.java
@@ -7,15 +7,12 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
